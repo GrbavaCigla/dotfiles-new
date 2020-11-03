dfb3315ab900133250bd92e83ea2a4299fc96471
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2020-11-03 15:07:23 +0100

    Htop, fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,10 @@
 set -x GPG_TTY (tty)
+
+alias hcat "pygmentize -g"
+alias ls "exa --color=always -F"
+alias grep "grep --color=always"
+alias less "less -r"
+alias la "exa --color=always -aF"
+alias l "exa --color=always -F"
+alias ll "exa --color=always -alF"
+
